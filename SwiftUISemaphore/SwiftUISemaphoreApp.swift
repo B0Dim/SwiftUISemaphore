9b809bd5cbd1240aa62c10bdcc851dc06b1df848
B0Dim <bodim@me.com>
2022-02-01 16:41:50 +0300

    Initial Commit

---
+++ SwiftUISemaphore/SwiftUISemaphoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISemaphoreApp.swift
+//  SwiftUISemaphore
+//
+//  Created by  BoDim on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISemaphoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
